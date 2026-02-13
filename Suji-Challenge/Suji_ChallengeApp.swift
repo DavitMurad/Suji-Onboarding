f7b9bc5c2dd527df847a757b674fd84566a460b1
“Davit <2840850M@student.gla.ac.uk>
2026-02-13 19:47:08 +0000

    Initial commit

---
+++ Suji-Challenge/Suji_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Suji_ChallengeApp.swift
+//  Suji-Challenge
+//
+//  Created by Findlay Wood on 09/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Suji_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
